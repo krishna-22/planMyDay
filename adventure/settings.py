47d5877f32b9221b11270625c7d2645db5f7c1bd
krishna-22 <42892516+krishna-22@users.noreply.github.com>
2023-03-23 19:54:15 -0400

    Update settings.py

--- adventure/settings.py
+++ adventure/settings.py
@@ -9,7 +9,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'goginenisaikiran31677@gmail.com'
-EMAIL_HOST_PASSWORD = 'vvtgzsnteimwtxii'
+EMAIL_HOST_PASSWORD = 'xxxxxxxxxx'
 DEFAULT_FROM_EMAIL = 'goginenisaikiran31677@gmail.com'
 
 
